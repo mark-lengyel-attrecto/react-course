24376e904e056a3a2085812ce6629b3f562cf84d
mark.lengyel <mark.lengyel@attrecto.com>
2024-04-19 15:57:35 +0200

    REA-002: Finishing todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
+import Todo from "./components/Todo";
+
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <div className="card">
-        <h2>TITLE</h2>
-        <div className="actions">
-          <button className="btn">Delete</button>
-        </div>
-      </div>
+      <Todo text="Learn react" />
+      <Todo text="Master react" />
+      <Todo text="Explore the full React course" />
     </div>
   );
 }
